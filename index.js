80495d4971b5365e9c5484f0696e7f24421717ed
Zer000s <znachok.egor@gmail.com>
2023-12-23 16:55:21 +0300

    second commit

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const app = express()
 app.use(express.json());
 
 app.get('/2d492367-2a0a-4b59-95be-e7c3f41cf514', async function (req, res) {
-    const db = await sqlite.open({filename: "./db.sqlite3", driver: sqlite3.cached.Database});
+    const db = await sqlite.open({filename: "./db.sqlite", driver: sqlite3.cached.Database});
     const data = await db.all(`SELECT * FROM first`);
     res.send(data);
 })
 
 app.post('/2d492367-2a0a-4b59-95be-e7c3f41cf514', async function (req, res) {
-    const db = await sqlite.open({filename: "./db.sqlite3", driver: sqlite3.cached.Database});
+    const db = await sqlite.open({filename: "./db.sqlite", driver: sqlite3.cached.Database});
     let mass = req.body;
     let sql = `UPDATE first
                 SET x = ?, y = ?
@@ -26,13 +26,13 @@ app.post('/2d492367-2a0a-4b59-95be-e7c3f41cf514', async function (req, res) {
 
 
 app.get('/f5f9048d-0204-4dfa-abb6-ab5006717100', async function (req, res) {
-    const db = await sqlite.open({filename: "./db.sqlite3", driver: sqlite3.cached.Database});
+    const db = await sqlite.open({filename: "./db.sqlite", driver: sqlite3.cached.Database});
     const data = await db.all(`SELECT * FROM second`);
     res.send(data);
 })
 
 app.post('/f5f9048d-0204-4dfa-abb6-ab5006717100', async function (req, res) {
-    const db = await sqlite.open({filename: "./db.sqlite3", driver: sqlite3.cached.Database});
+    const db = await sqlite.open({filename: "./db.sqlite", driver: sqlite3.cached.Database});
     let mass = req.body;
     let sql = `UPDATE second
                 SET x = ?, y = ?
@@ -46,13 +46,13 @@ app.post('/f5f9048d-0204-4dfa-abb6-ab5006717100', async function (req, res) {
 
 
 app.get('/8192daaf-059a-4660-8405-87eb4c32e669', async function (req, res) {
-    const db = await sqlite.open({filename: "./db.sqlite3", driver: sqlite3.cached.Database});
+    const db = await sqlite.open({filename: "./db.sqlite", driver: sqlite3.cached.Database});
     const data = await db.all(`SELECT * FROM thirtd`);
     res.send(data);
 })
 
 app.post('/8192daaf-059a-4660-8405-87eb4c32e669', async function (req, res) {
-    const db = await sqlite.open({filename: "./db.sqlite3", driver: sqlite3.cached.Database});
+    const db = await sqlite.open({filename: "./db.sqlite", driver: sqlite3.cached.Database});
     let mass = req.body;
     let sql = `UPDATE thirtd
                 SET x = ?, y = ?
